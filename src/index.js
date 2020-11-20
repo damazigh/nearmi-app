5a9e9d2d324b353e38abff8e1c2add97f7b255c5
Amazigh Djebarri <djebarri.amazigh@gmail.com>
2020-11-20 15:05:16 +0100

    removing aws

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import config from './aws-exports';
-import Amplify from 'aws-amplify';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-Amplify.configure(config);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
