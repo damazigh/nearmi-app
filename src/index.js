0320a1041a190253c878d020c5676ba2c1c5f5d1
Amazigh Djebarri <djebarri.amazigh@gmail.com>
2020-11-20 16:09:27 +0100

    (je me suis chié dans la colle) j'ai pas push la fin du rebase

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import AmplifyBridge from './redux/amplify-bridge';
 
 ReactDOM.render(
   <React.StrictMode>
